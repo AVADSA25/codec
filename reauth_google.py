a9595aa7dce5f010158440faca8b40de2e75868c
Mickael Farina <farina.mickael@gmail.com>
2026-03-27 23:34:56 +0100

    v1.3.0 — Google OAuth upgraded to read+write scopes

--- reauth_google.py
+++ reauth_google.py
@@ -6,13 +6,13 @@ CREDS_PATH = os.path.expanduser("~/.codec/google_credentials.json")
 TOKEN_PATH = os.path.expanduser("~/.codec/google_token.json")
 
 SCOPES = [
-    "https://www.googleapis.com/auth/gmail.readonly",
-    "https://www.googleapis.com/auth/calendar.readonly",
-    "https://www.googleapis.com/auth/drive.readonly",
-    "https://www.googleapis.com/auth/documents.readonly",
-    "https://www.googleapis.com/auth/spreadsheets.readonly",
-    "https://www.googleapis.com/auth/presentations.readonly",
-    "https://www.googleapis.com/auth/tasks.readonly"
+    "https://www.googleapis.com/auth/gmail.modify",
+    "https://www.googleapis.com/auth/calendar",
+    "https://www.googleapis.com/auth/drive",
+    "https://www.googleapis.com/auth/documents",
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/presentations",
+    "https://www.googleapis.com/auth/tasks"
 ]
 
 # Delete old token to force re-auth
